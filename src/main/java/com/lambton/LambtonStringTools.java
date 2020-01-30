02d61c00552f998fa4cb92fe8ef1d169c8d06bae
macstudent <neeliadithyasai@gmail.com>
2020-01-29 19:41:35 -0500

    [Adithyasai] reversestring 2

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -5,12 +5,16 @@
  */
 package com.lambton;
 
+import javax.print.DocFlavor;
+import java.util.HashMap;
+
 /**
  *
  * @author Pritesh Patel
  */
-public class LambtonStringTools 
+public class LambtonStringTools
 {
+
     //1 - REVERSE STRING
     public static String reverse(String s)
     {
@@ -20,42 +24,66 @@ public class LambtonStringTools
         }
         else {
 
-
-            String n[] = s.split("\\s+");
-            String temp = " ";
-            for (int i = n.length - 1; i > 0; i--)
+            String splittedArray[]= s.split("\\s+");
+            String temp=" ";
+            for (int i=splittedArray.length-1;i>0;i--)
             {
-                temp = temp + n[i] + " ";
+                temp=temp+splittedArray[i]+" ";
             }
+            return temp;
         }
-
-        return temp;
     }
 
-
-    
     //2 - FORMAT INITIALS OF STRING
     public static String initials(String s)
     {
-
         return null;
     }
 
     //3 - FIND MOST FREQUENT CHARACTER FROM STRING
     public static String mostFrequent(String s)
     {
-        return null;
+        char temp[]=s.toCharArray();
+
+        HashMap<Character,Integer> frequentLetters = new HashMap<Character, Integer>();
+
+        for (int i=0;i<s.length();i++)
+        {
+            if (frequentLetters.containsKey(temp[i]))
+            {
+                int tempValue= frequentLetters.get(temp[i]);
+                tempValue++;
+                frequentLetters.put(temp[i],tempValue);
+            }
+            else
+            {
+                frequentLetters.put(temp[i],1);
+            }
+        }
+
+
+
+        /*for (int a:frequentLetters.values())
+        {
+            System.out.println(a);
+        }*/
+        return  null;
     }
 
     //4 - CONVERT BINARY NUMBER TO DECIMALS
     public static int binaryToDecimal(String s)
     {
-       return 0;
+
+
+
+
+        return 0;
     }
 
     //5 - REPLACING SUBSTRING WITH NEW STRING IN EXISTING STRING
     public static String replaceSubString(String originalString, String findString, String newString)
     {
+
         return null;
     }
 }
